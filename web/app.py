b9a4ef5029788291b0f9d6105aa4d656e7976060
Moesen <gustavmoesmand@gmail.com>
2022-04-05 21:38:33 +0200

    add lorem ipsum to frontpage

--- web/app.py
+++ web/app.py
@@ -1,5 +1,5 @@
-from dash import Dash, Input, Output, dcc, html
 import dash_bootstrap_components as dbc
+from dash import Dash, Input, Output, dcc, html
 
 external_stylesheets = [dbc.themes.LUX]
 
@@ -8,8 +8,16 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-   html.Header("Test")
-], style={"display": "flex", "justify-content": "center"})
+    html.Header([
+        html.H1("Pedestrians in Melbourne AU")
+        ], style = {"margin-top": "1.5rem"}
+    ),
+    dcc.Markdown(["---------------------------------",
+                  "#### **Overview of the data**",
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                  ]),
+    ], style={"display": "flex", "align-items": "center", "flex-direction": "column", "max-width": "45vw", "margin": "0 auto"}
+)
 
 
 if __name__ == "__main__":

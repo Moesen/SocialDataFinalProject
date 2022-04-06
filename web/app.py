619da500d9e93a351f90fa01367bf44565e84ffc
Moesen <gustavmoesmand@gmail.com>
2022-04-06 12:10:15 +0200

    added monthly patterns dataset

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 import dash_bootstrap_components as dbc
-from dash import Dash, Input, Output, dcc, html
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
+from dash import Dash, Input, Output, dcc, html
 
 # Setup of app
 external_stylesheets = [dbc.themes.LUX]
@@ -12,35 +12,54 @@ server = app.server
 
 # Reading locations and counts for locations
 df_locations = pd.read_csv("data/locations.csv")
-fig = px.scatter_mapbox(df_locations,
-                     lat="latitude_sensor",
-                     lon="longitude_sensor",
-                     hover_data={'latitude_sensor': False,
-                                 "longitude_sensor": False,
-                                 "count": ":100.0f"},
-                     color_discrete_sequence=["darkgreen"],
-                     size="count",
-                     size_max=20,
-                     zoom=13)
+fig = px.scatter_mapbox(
+    df_locations,
+    lat="latitude_sensor",
+    lon="longitude_sensor",
+    hover_data={
+        "latitude_sensor": False,
+        "longitude_sensor": False,
+        "count": ":100.0f",
+    },
+    color_discrete_sequence=["darkgreen"],
+    size="count",
+    size_max=20,
+    zoom=13,
+)
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 
-app.layout = html.Div([
-    html.Header([
-        html.H1("Pedestrians in Melbourne AU")
-        ], style = {"margin-top": "1.5rem"}
-    ),
-    dcc.Markdown(["---------------------------------",
-                  "#### **Overview of the data**",
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                  ]),
-    dcc.Markdown(["----------",
-                  "#### **Sensor locations**",
-                  "This is a map of the different pedestrian sensors, and the activity they have tracked throughout the period"
-                  ]),
-    dcc.Graph(figure=fig, style={"width": "100%"})
-    ], style={"display": "flex", "align-items": "center", "flex-direction": "column", "max-width": "45vw", "margin": "0 auto"}
+app.layout = html.Div(
+    [
+        html.Header(
+            [html.H1("Pedestrians in Melbourne AU")], style={"margin-top": "1.5rem"}
+        ),
+        dcc.Markdown(
+            [
+                "---------------------------------",
+                "#### **Introduction to our datasets**",
+                "We have chosen to work with the lovely city of melbourne.",
+                "Here we are focusing on ...\n",
+                "From the [City of melbourne - Open data](https://data.melbourne.vic.gov.au/) website we found these datasets particularely interesting"
+            ]
+        ),
+        dcc.Markdown(
+            [
+                "----------",
+                "#### **Sensor locations**",
+                "This is a map of the different pedestrian sensors, and the activity they have tracked throughout the period",
+            ]
+        ),
+        dcc.Graph(figure=fig, style={"width": "100%"}),
+    ],
+    style={
+        "display": "flex",
+        "align-items": "center",
+        "flex-direction": "column",
+        "max-width": "45vw",
+        "margin": "0 auto",
+    },
 )
 
 
